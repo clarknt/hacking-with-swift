d296795230cd009d4488f1a034cbffd9d5a7d8a4
clarknt <24315769+clarknt@users.noreply.github.com>
2019-09-24 23:53:04 -0400

    Init project 31

---
+++ 01-Project31/Project31/Project31/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Project31
+//
+//  Created by clarknt on 2019-09-24.
+//  Copyright © 2019 clarknt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var addressBar: UITextField!
+    @IBOutlet weak var stackView: UIStackView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
