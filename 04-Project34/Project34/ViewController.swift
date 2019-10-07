d810ffe53622c34b4129553a4b1901e86be3863b
clarknt <24315769+clarknt@users.noreply.github.com>
2019-10-07 14:08:11 -0400

    Project 34 - First part

---
+++ 04-Project34/Project34/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Project34
+//
+//  Created by clarknt on 2019-10-07.
+//  Copyright © 2019 clarknt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var columnButtons: [UIButton]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func makeMove(_ sender: UIButton) {
+    }
+}
+
