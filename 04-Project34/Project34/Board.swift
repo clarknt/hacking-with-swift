92bcf05b30990d35e3050d7259a7348f3a9b3cfc
clarknt <24315769+clarknt@users.noreply.github.com>
2019-10-07 14:55:00 -0400

    Project 34 - Part 3

---
+++ 04-Project34/Project34/Board.swift
@@ -0,0 +1,55 @@
+//
+//  Board.swift
+//  Project34
+//
+//  Created by clarknt on 2019-10-07.
+//  Copyright © 2019 clarknt. All rights reserved.
+//
+
+import UIKit
+
+enum ChipColor: Int {
+    case none = 0
+    case red
+    case black
+}
+class Board: NSObject {
+    static var width = 7
+    static var height = 6
+
+    var slots = [ChipColor]()
+
+    override init() {
+        for _ in 0 ..< Board.width * Board.height {
+            slots.append(.none)
+        }
+    }
+
+    func chip(inColumn column: Int, row: Int) -> ChipColor {
+        return slots[row + column * Board.height]
+    }
+
+    func set(chip: ChipColor, in column: Int, row: Int) {
+        slots[row + column * Board.height] = chip
+    }
+
+    func nextEmptySlot(in column: Int) -> Int? {
+        for row in 0 ..< Board.height {
+            if chip(inColumn: column, row: row) == .none {
+                return row
+            }
+        }
+
+        return nil
+    }
+
+    func canMove(in column: Int) -> Bool {
+        return nextEmptySlot(in: column) != nil
+    }
+
+    func add(chip: ChipColor, in column: Int) {
+        if let row = nextEmptySlot(in: column) {
+            set(chip: chip, in: column, row: row)
+        }
+    }
+}
--- 04-Project34/Project34/ViewController.swift
+++ 04-Project34/Project34/ViewController.swift
@@ -12,12 +12,76 @@ class ViewController: UIViewController {
 
     @IBOutlet var columnButtons: [UIButton]!
 
+    var placedChips = [[UIView]]()
+    var board: Board!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        for _ in 0 ..< Board.width {
+            placedChips.append([UIView]())
+        }
+
+        resetBoard()
+    }
+
+    func resetBoard() {
+        board = Board()
+
+        for i in 0 ..< placedChips.count {
+            for chip in placedChips[i] {
+                chip.removeFromSuperview()
+            }
+
+            placedChips[i].removeAll(keepingCapacity: true)
+        }
+    }
+
+    func addChip(inColumn column: Int, row: Int, color: UIColor) {
+        let button = columnButtons[column]
+        let size = getSize(forButton: button)
+        let rect = CGRect(x: 0, y: 0, width: size, height: size)
+
+        if placedChips[column].count < row + 1 {
+            let newChip = UIView()
+            newChip.frame = rect
+            newChip.isUserInteractionEnabled = false
+            newChip.backgroundColor = color
+            newChip.layer.cornerRadius = size / 2
+            newChip.center = positionForChip(inColumn: column, row: row)
+            newChip.transform = CGAffineTransform(translationX: 0, y: -800)
+            view.addSubview(newChip)
+
+            UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseIn, animations: {
+                newChip.transform = CGAffineTransform.identity
+            })
+
+            placedChips[column].append(newChip)
+        }
+    }
+
+    func getSize(forButton button: UIButton) -> CGFloat {
+        return min(button.frame.width, button.frame.height / CGFloat(Board.height))
+    }
+
+    func positionForChip(inColumn column: Int, row: Int) -> CGPoint {
+        let button = columnButtons[column]
+        let size = getSize(forButton: button)
+
+        let xOffset = button.frame.midX
+        var yOffset = button.frame.maxY - size / 2
+        yOffset -= size * CGFloat(row)
+
+        return CGPoint(x: xOffset, y: yOffset)
     }
 
     @IBAction func makeMove(_ sender: UIButton) {
+        let column = sender.tag
+
+        if let row = board.nextEmptySlot(in: column) {
+            board.add(chip: .red, in: column)
+            addChip(inColumn: column, row: row, color: .red)
+        }
     }
 }
 
