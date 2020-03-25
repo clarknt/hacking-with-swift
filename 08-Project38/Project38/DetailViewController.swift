0323de73ee1357ba9dd6321a7f23fcf3bab06ed5
clarknt <24315769+clarknt@users.noreply.github.com>
2020-03-24 22:45:40 -0400

    Project 38 - Init

---
+++ 08-Project38/Project38/DetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.swift
+//  Project38
+//
+//  Created by clarknt on 2020-03-24.
+//  Copyright © 2020 clarknt. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 08-Project38/Project38/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project38
+//
+//  Created by clarknt on 2020-03-24.
+//  Copyright © 2020 clarknt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
