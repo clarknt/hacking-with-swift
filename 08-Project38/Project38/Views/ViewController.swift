54778b6c260496776c1bcc5075f26588abb15a31
clarknt <24315769+clarknt@users.noreply.github.com>
2020-03-25 17:17:53 -0400

    Project 38 - Add CD

--- 08-Project38/Project38/Views/ViewController.swift
+++ 08-Project38/Project38/Views/ViewController.swift
@@ -6,15 +6,33 @@
 //  Copyright © 2020 clarknt. All rights reserved.
 //
 
+import CoreData
 import UIKit
 
 class ViewController: UITableViewController {
 
+    var container: NSPersistentContainer!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        container = NSPersistentContainer(name: "Project38")
+
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error {
+                print("Unresolved error \(error)")
+            }
+        }
     }
 
-
+    func saveContext() {
+        if container.viewContext.hasChanges {
+            do {
+                try container.viewContext.save()
+            } catch {
+                print("An error occurred while saving: \(error)")
+            }
+        }
+    }
 }
 
