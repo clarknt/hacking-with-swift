6094bfec5cc73be62cbcdcc9250c7a23d677cd92
clarknt <24315769+clarknt@users.noreply.github.com>
2020-03-30 17:08:22 -0400

    Project 38 - Challenge 1

---
+++ 08-Project38/Challenges/Project38-Challenge1/Project38-Challenge1/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Project38-Challenge1
+//
+//  Created by clarknt on 2020-03-30.
+//  Copyright © 2020 clarknt. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
