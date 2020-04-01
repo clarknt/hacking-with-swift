a390320be59343700298b71fa330d7e8f1d0fd21
clarknt <24315769+clarknt@users.noreply.github.com>
2020-04-01 16:48:19 -0400

    Project 38 - Challenge 2

--- 08-Project38/Project38/Controllers/DetailViewController.swift
+++ 08-Project38/Project38/Controllers/DetailViewController.swift
@@ -7,19 +7,54 @@
 //
 
 import UIKit
+import WebKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailLabel: UILabel!
+    // challenge 2
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
 
     var detailItem: Commit?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // challenge 2
+        activityIndicator.hidesWhenStopped = true
+        webView.navigationDelegate = self
+
         if let detail = self.detailItem {
-            detailLabel.text = detail.message
             // navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Commit 1/\(detail.author.commits.count)", style: .plain, target: self, action: #selector(showAuthorCommits))
+
+            // challenge 2
+            if let url = URL(string: detail.url) {
+                webView.load(URLRequest(url: url))
+                webView.allowsBackForwardNavigationGestures = true
+            }
         }
+    }
+}
+
+// challenge 2
+extension DetailViewController: WKNavigationDelegate {
+    func showActivityIndicator(show: Bool) {
+        if show {
+            activityIndicator.startAnimating()
+        } else {
+            activityIndicator.stopAnimating()
+        }
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        showActivityIndicator(show: false)
+    }
+
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        showActivityIndicator(show: true)
+    }
+
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        showActivityIndicator(show: false)
     }
 }
