5b6896d4c33860f637992fb1b4d5067756852ac8
clarknt <24315769+clarknt@users.noreply.github.com>
2020-04-08 12:22:39 -0400

    Project 39 - Part 2

---
+++ 09-Project39/Project39/PlayData.swift
@@ -0,0 +1,13 @@
+//
+//  PlayData.swift
+//  Project39
+//
+//  Created by clarknt on 2020-04-08.
+//  Copyright © 2020 clarknt. All rights reserved.
+//
+
+import Foundation
+
+class PlayData {
+    var allWords = [String]()
+}
--- 09-Project39/Project39Tests/Project39Tests.swift
+++ 09-Project39/Project39Tests/Project39Tests.swift
@@ -19,16 +19,8 @@ class Project39Tests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAllWordsLoaded() {
+        let playData = PlayData()
+        XCTAssertEqual(playData.allWords.count, 0, "allWords must be 0")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
