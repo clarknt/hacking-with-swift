40509c37cf909ef19bce393eb2433af9717af050
clarknt <24315769+clarknt@users.noreply.github.com>
2020-04-08 11:57:00 -0400

    Project 39 - Setting up

---
+++ 09-Project39/Project39/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project39
+//
+//  Created by clarknt on 2020-04-08.
+//  Copyright © 2020 clarknt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 09-Project39/Project39Tests/Project39Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Project39Tests.swift
+//  Project39Tests
+//
+//  Created by clarknt on 2020-04-08.
+//  Copyright © 2020 clarknt. All rights reserved.
+//
+
+import XCTest
+@testable import Project39
+
+class Project39Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
